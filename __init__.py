6f8657b8f5c21e4c8b05116656e0ae0237c3e517
gokayfem <88277926+gokayfem@users.noreply.github.com>
2024-01-26 14:51:18 +0300

    modify init

--- __init__.py
+++ __init__.py
@@ -9,7 +9,7 @@ NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
 
 for module_name in node_list:
-    imported_module = importlib.import_module(f".{module_name}", __name__)
+    imported_module = importlib.import_module(f".nodes.{module_name}", __name__)
 
     NODE_CLASS_MAPPINGS = {**NODE_CLASS_MAPPINGS, **imported_module.NODE_CLASS_MAPPINGS}
     NODE_DISPLAY_NAME_MAPPINGS = {**NODE_DISPLAY_NAME_MAPPINGS, **imported_module.NODE_DISPLAY_NAME_MAPPINGS}
