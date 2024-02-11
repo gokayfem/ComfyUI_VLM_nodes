259bfe839f734ac61f1e45cb21ac057c3bb5d897
gokayfem <civileng.gokay@gmail.com>
2024-02-11 18:34:17 +0300

    ViewText

--- nodes/simpletext.py
+++ nodes/simpletext.py
@@ -68,10 +68,34 @@ class JsonToText:
         formatted_output_str = "\n\n".join(merged_ideas)
         return {"ui": {"text": formatted_output_str}, "result": (formatted_output_str,)}
 
+class ViewText:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text": ("STRING", {"forceInput": True}),
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "view_text"
+    OUTPUT_NODE = True
+
+    CATEGORY = "VLM Nodes/Text"
+
+    def view_text(self, text):
+        # Parse the combined JSON string
+        return {"ui": {"text": text}, "result": (text,)}
+
 # A dictionary that contains all nodes you want to export with their names
 NODE_CLASS_MAPPINGS = {"SimpleText": SimpleText,
-                       "JsonToText": JsonToText}
+                       "JsonToText": JsonToText,
+                       "ViewText": ViewText}
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {"SimpleText": "SimpleText",
-                              "JsonToText": "JsonToText"}
+                              "JsonToText": "JsonToText",
+                              "ViewText": "ViewText"}
