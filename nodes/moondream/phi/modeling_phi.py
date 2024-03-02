3d72d9bbd193f4066cd438e0634ae0c6782f0760
gokayfem <civileng.gokay@gmail.com>
2024-03-02 18:31:27 +0300

    moondream fix

--- nodes/moondream/phi/modeling_phi.py
+++ nodes/moondream/phi/modeling_phi.py
@@ -459,6 +459,7 @@ class CrossAttention(nn.Module):
                 dtype=scores.dtype,
                 device=scores.device,
             )
+            key_padding_mask = key_padding_mask[:, :seqlen_k]
             padding_mask.masked_fill_(key_padding_mask, 0.0)
 
             scores = scores + rearrange(padding_mask, "b s -> b 1 1 s")
