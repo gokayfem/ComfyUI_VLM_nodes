5779fbb711006ced08a8aca3fe1e6e7fc7fcf8b6
gokayfem <civileng.gokay@gmail.com>
2024-03-06 14:15:35 +0300

    pycpuinfo

--- install_init.py
+++ install_init.py
@@ -20,50 +20,6 @@ from tqdm import tqdm
 import pkg_resources
 
 
-
-def get_python_version():
-    """Return the Python version in a concise format, e.g., '39' for Python 3.9."""
-    version_match = re.match(r"3\.(\d+)", platform.python_version())
-    if version_match:
-        return "3" + version_match.group(1)
-    else:
-        return None
-
-def get_system_info():
-    """Gather system information related to NVIDIA GPU, CUDA version, AVX2 support, Python version, OS, and platform tag."""
-    system_info = {
-        'gpu': False,
-        'cuda_version': None,
-        'avx2': False,
-        'python_version': get_python_version(),
-        'os': platform.system(),
-        'os_bit': platform.architecture()[0].replace("bit", ""),
-        'platform_tag': None,
-    }
-
-    # Check for NVIDIA GPU and CUDA version
-    if importlib.util.find_spec('torch'): 
-        system_info['gpu'] = torch.cuda.is_available()
-        if system_info['gpu']:
-            system_info['cuda_version'] = "cu" + torch.version.cuda.replace(".", "").strip()
-    
-    # Check for AVX2 support
-    if importlib.util.find_spec('cpuinfo'):        
-        system_info['avx2'] = 'avx2' in cpuinfo.get_cpu_info().get('flags',[])
-
-    # Determine the platform tag
-    if importlib.util.find_spec('packaging.tags'):        
-        system_info['platform_tag'] = next(packaging.tags.sys_tags()).platform
-
-    return system_info
-
-def latest_lamacpp():
-    try:        
-        response = get("https://api.github.com/repos/abetlen/llama-cpp-python/releases/latest")
-        return response.json()["tag_name"].replace("v", "")
-    except Exception:
-        return "0.2.20"
-
 def install_package(package_name, custom_command=None):
     if not package_is_installed(package_name):
         print(f"Installing {package_name}...")
